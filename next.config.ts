8e9a5f4fc7fbb6caae034d9b197915567da29279
jiyoon <wldbs4282@gmail.com>
2025-12-19 00:44:54 +0900

    이미지 도메인 추가 (takaratomy-arts)

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,10 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "search.pstatic.net" },
       { protocol: "https", hostname: "mblogthumb-phinf.pstatic.net" },
+      { protocol: "https", hostname: "ldb-phinf.pstatic.net" },
       { protocol: "https", hostname: "gashapon.jp" },
       { protocol: "https", hostname: "bandai-a.akamaihd.net" },
+      { protocol: "https", hostname: "www.takaratomy-arts.co.jp" },
     ],
   },
 };
